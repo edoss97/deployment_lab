fbb8de2dd2b46180367ef8e0f3d99cdcb4f0312c
Erik Doss <edoss@umich.edu>
2022-12-15 14:11:00 -0500

    rollbar add

--- server/server.js
+++ server/server.js
@@ -4,6 +4,14 @@ const path=require('path')
 
 const app = express()
 
+var Rollbar = require('rollbar')
+var rollbar = new Rollbar({
+  accessToken: '79ce65d781d6429f9e933c5bc727d091',
+  captureUncaught: true,
+  captureUnhandledRejections: true,
+})
+rollbar.log('Hello world!')
+
 app.use(cors())
 app.use(express.json())
 app.use(express.static('public'))
